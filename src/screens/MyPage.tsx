fa6c0bb44d8decfa5558192627f8a0450f8fa80b
find11570 <find11570@kumoh.ac.kr>
2022-03-27 22:28:42 +0900

    front 1차 검증 코드

---
+++ src/screens/MyPage.tsx
@@ -0,0 +1,12 @@
+
+import React from 'react'
+import {SafeAreaView} from '../theme'
+const title = 'MyPage'
+
+export default function MyPage() {
+  return (
+    <SafeAreaView>
+      
+    </SafeAreaView>
+  )
+}
